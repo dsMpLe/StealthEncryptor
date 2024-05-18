16101192e637e89c86f376ecbb2f7a8b2b6e0a0c
dsMpLe <dennisortlieb13@gmail.com>
2024-05-18 13:55:59 +0200

    update

--- client.py
+++ client.py
@@ -1,7 +1,24 @@
 import socket
 import os
-
+from time import sleep
 CHUNKSIZE = 1_000_000
 
 sock = socket.socket()
 sock.connect(('localhost', 33333))
+
+filepath = input("Please type in the full path to your file you want to send\nSyntax: C:/folder/.../file\n")
+
+if filepath == None:
+    print("Something went wrong. Please check your syntax!")
+    print("Program stopped!")
+    exit(1)
+
+sock.sendall(filepath.encode())
+sleep(1)
+file = open(filepath, "r")
+data = file.read(1024)
+while data:
+    sock.send(data.encode())
+    data = file.read(1024)
+    
+print("sendfile.txt ist leer")
--- server.py
+++ server.py
@@ -1,20 +1,36 @@
 import socket
 import os
 
-socket.setdefaulttimeout(1) 
+socket.setdefaulttimeout(10) 
 
-CHUNKSIZE = 1_000_000
+#CHUNKSIZE = 1_000_000
 
+#socket object initiated
 sock = socket.socket()
+#socket bind on localhost:33333
 sock.bind(("", 33333))
+#socket listens and one connection is allowed
 sock.listen(1)
 
 print("Waiting for connection ...")
 
 while True:
     try:
+        #client = socket information & addr is only the ip address
         client, addr = sock.accept()
         print("connected with {}".format(addr))
+        filename = client.recv(1600).decode()
+        
+        f = open(filename, "w")
+        
+        data = client.recv(1024).decode()
+        while data:
+            
+            f.writelines(str(data))
+            data = client.recv(1024).decode()
+            
+        f.close()    
+        print("Fertig gelesen")
 
     except socket.timeout:
         print("Waiting for connection ...")
