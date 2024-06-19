e09fd108aedec00a23c62c72e46df38e5ceda541
dsMpLe <dennisortlieb13@gmail.com>
2024-06-19 22:19:40 +0200

    update: sending txt files from folder

--- client.py
+++ client.py
@@ -5,24 +5,34 @@ from time import sleep
 CHUNKSIZE = 1_000_000
 
 while True:
-    sock = socket.socket()
-    sock.connect(('localhost', 33333))
-
-    filepath = input("Please type in the full path to your file you want to send\nSyntax: C:/folder/.../file\n")
-
-    if filepath == None:
+    
+    
+    path = input("Please type in the full path to your folder you want to send\nSyntax: C:/folder/.../file\n")
+    
+    
+    
+    if path == None:
         print("Something went wrong. Please check your syntax!")
         print("Program stopped!")
         exit(1)
 
-    sock.sendall(filepath.encode())
-    sleep(3)
-    file = open(filepath, "r")
-    data = file.read(1024)
-    while data:
-        sock.send(data.encode())
+    files = os.listdir(path)
+    txt_files = [file for file in files if file.endswith(".txt")]
+    
+    
+    
+    for file in txt_files:
+        sock = socket.socket()
+        sock.connect(('localhost', 33333))
+        file = path + "/" + file
+        sock.sendall(file.encode())
+        sleep(3)
+        file = open(file, "r")
         data = file.read(1024)
-        
-    print("{} ist leer".format(filepath))
-    sleep(3)
-    sock.close()
+        while data:
+            sock.send(data.encode())
+            data = file.read(1024)
+            
+        print("{} ist leer".format(file))
+        sleep(3)
+        sock.close()
